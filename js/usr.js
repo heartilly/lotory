ab995404415f7f040093d1ebfc128dae8dc23645
linux cat <linux.cat.me@gmail.com>
2011-06-06 00:09:27 +0800

    ajax sucess, phase detail

--- js/usr.js
+++ js/usr.js
@@ -1,37 +1,42 @@
-var 	SITE_INFO ={
-		pmp: {
+var 	_SITE ={
+		PMP: {
+			nn:"PMP",
 			url:'http://www.pmp.com.my/english/1_3d/3D_main.asp',
 			charset:'iso-8859-1',
 			pmpDrawDateVari:3,
 			pmpResultVari:26
 		},
-		sg4dlastest: {
+		SG4DN: {
+			nn:"SG4D",
 			url:'ddf',
 			charset:'iso-8859-1',
 			sg4dLastestDateVari:1,
 			sg4dResultTopVari:3,
 			sg4dResultVari:20
 		},
-		sg4d: {
+		SG4D: {
+			nn:"",
 			url:'http://www.singaporepools.com.sg/Lottery?page=four_d',
 			charset:'iso-8859-1'
 		},
-		magnum: {
+		MAG4D: {
+			nn:"MAG4D",
 			url:'http://www.live4d.com/live4d/4dlive1.htm',
 			charset:'UTF-16',
+			xObj:'strong',			
 			xpath:'//strong',
+			xCheck:33
 			// YQL
 			// select * from html where url="http://www.live4d.com/live4d/4dlive1.htm" AND xpath="//strong" AND charset="UTF-16"
 			// Verificasion condition
-			magnumDrawNoVari:10,
-			magnumTop3Vari:3,
-			magnumReusltVari:2
 		},
-		toto:{
+		TOTO:{
+			nn:"d",
 			url:'http://www.sportstoto.com.my/g_past_results/main.asp',
 			charset:'UTF-8',
 		},
-		totolastest: {
+		TOTON: {
+			nn:"TOTO",
 			url:'totourl',
 			charset:'UTF-8',
 			// Verificasion condition
@@ -70,10 +75,18 @@ var 	SITE_INFO ={
        	}
 		);
 	},
+	xchk : function(SITE,objLength){
+		if(objLength===SITE.xCheck){
+			console.log("@ "+SITE.nn +"   x-check PASSED");
+			
+		}
+		else {alert(SITE.nn +" Verify Error");throw new Error('x-check Failured, object = '+objLength+' but given object = '+SITE.xCheck+', please contact programer.');}
+	},
 	success : function(results){
-	
-		
+		ajx.prototype.results = results; 
 		console.log(results)
+		console.log("ax.results = " + this.results)
+		console.log("ax = " + this)
 	},
 	load1 : function() {
 		aj.requestCrossDomain(SITE_INFO.magnum.url,SITE_INFO.magnum.xpath,SITE_INFO.magnum.charset,function(results){
@@ -85,32 +98,47 @@ var 	SITE_INFO ={
 		$('#test').ajaxStart(function(){
  		  $(this).hide();
 		 });
-		var ax = new ajx(SITE_INFO.magnum);
-		//$.get('page.html&callback=?',function(d){alert(d)});
+		var SITE = _SITE.MAG4D,
+			ax = new ajx(SITE, function(data){
+			// Virify		
+			var i = data.strong.length,obj = data.strong.reverse();
+			aj.xchk(SITE,i);
+      			console.log(obj)
+      	while(i--) {
+      		if(i){}
+      		}
+			console.log("ax.results = " + ax.results)
+			console.log("ax = " + ax.length)
+			},"data.strong");
 		console.log(ax.site);
-		console.log(ax.yql);
-		//console.log(ax.ajxr);
 		/* * /
 		aj.requestCrossDomain(ax.yql,function(results){
 			console.log(results);
 			})
 		/* */
+		console.log(ax.yql);
 		
 	}
 	},
-	ajx = function(SITE){
-		this.site = SITE;
+	ajx = function(SITE,callback,xyz){
+		//this.site = SITE;
        	this.yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + SITE.url + '" AND xpath="'+ SITE.xpath +'" AND charset="'+SITE.charset+'"') +'&format=json&callback=?';
-       	/* * /
-       	this.ajxr = aj.requestCrossDomain(this.yql,function(results){
-       		//console.log(results);
-			return results;
-			})
-		/* */
-       	/* */
-       	this.ajxr = aj.requestCrossDomain(this.yql,aj.success)
-		/* */
-		}
+       	this.ajxr = $.getJSON( this.yql, function cbFunc(data) {
+				  	//console.log(data.query.results);
+				  	if (data.query.results) {
+						data = data.query.results;
+						// If the user passed a callback, and it
+							callback(data);
+       					}
+				  
+					// Else, Maybe we requested a site that doesn't exist, and nothing returned.
+					else throw new Error('Nothing returned from getJSON.');
+			   		});
+		
+		   		},
+	rslt = function(){
+		
+	}
 	
 	
 aj.load2()
